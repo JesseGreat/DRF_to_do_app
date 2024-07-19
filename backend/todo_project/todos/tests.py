b1862aacbaa587b5cf7a823ca2a24c698c6687bf
JesseGreat <oluwatobilobaokunogbe@gmail.com>
2024-07-19 12:06:00 -0700

    done with all the endpoints for todo list

---
+++ backend/todo_project/todos/tests.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from .models import Todo
+# Create your tests here.
+
+class TodoModelTest(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls):
+        Todo.objects.create(tittle='first todo', body='a body here')
+    
+    def test_tittle_content(self):
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f'{todo.tittle}'
+        self.assertEqual(expected_object_name, 'first todo')
+        
+    def test_body_content(self):
+        todo = Todo.objects.get(id=1)
+        expected_object_name = f'{todo.body}'
+        self.assertEqual(expected_object_name, 'a body here')
+        
